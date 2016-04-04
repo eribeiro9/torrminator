d8a2e7a19fe3e4f6ee88f5502c1ff76cdd403f90
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2016-04-03 22:35:01 -0400

    Better home page

--- client/components/areas/home.jsx
+++ client/components/areas/home.jsx
@@ -1,7 +1,25 @@
 Home = React.createClass({
+  searchTorrent(e) {
+    e.preventDefault()
+
+    console.log(e.target.search.value)
+
+    e.target.search.value = ''
+  },
+
   render() {
     return (
-      <h1>Torrminator</h1>
+      <div id="home-layout" className="ui centered grid">
+        <div className="eight wide column">
+          <h1 id="home-header" className="ui center aligned header">Torrminator</h1>
+          <form id="home-form" onSubmit={ this.searchTorrent } className="ui form">
+            <div className="ui big transparent fluid icon input">
+              <input type="text" name="search" placeholder="Find a torrent..." />
+              <i className="search icon"></i>
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 })
